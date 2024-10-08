19d5b204adbbaf6b3883b8de932743842cc98334
Lcs-ns <lucas.nas314@gmail.com>
2024-10-08 15:56:00 -0300

    feature: add project code

---
+++ src/githubUserActivityApp.java
@@ -0,0 +1,37 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class githubUserActivityApp {
+    public static void main(String[] args) {
+        if (args.length != 1){
+            System.out.println("Usage: java githubUserActivityApp <username>");
+            return;
+        }
+
+        String URL = "https://api.github.com/users/"+ args[0] + "/events";
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        try{
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(URL)).header("Accept", "application/vnd.github+json").GET().build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+            if (response.statusCode() == 200){
+                System.out.println(response);
+            } else {
+                System.out.println("Error:" + response.statusCode());
+            }
+        } catch (URISyntaxException uriSyntaxException){
+            uriSyntaxException.printStackTrace();
+        } catch (IOException ioException){
+            ioException.printStackTrace();
+        } catch (InterruptedException interruptedException){
+            Thread.currentThread().interrupt();
+            interruptedException.printStackTrace();
+        }
+    }
+}
